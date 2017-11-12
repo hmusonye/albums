4c626f42bbb55d0a1674a92d897d64046f81af94
andela-hms <humphrey.musonye@andela.com>
2017-11-12 22:54:42 +0300

    Generate boilerplate code

---
+++ albums/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+// import App from './App';
+
+const App = () => (
+    <Text>  Text </Text>
+);
+AppRegistry.registerComponent('albums', () => App);
