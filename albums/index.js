edcdc824c9d39a135c26b8529de77e4e45ea9d11
andela-hms <humphrey.musonye@andela.com>
2017-11-13 13:58:50 +0300

    (chore): Setup boilerplate code - Add a header and styling for it

--- albums/index.js
+++ albums/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 // import App from './App';
+import Header from './src/components/header';
 
 const App = () => (
-    <Text>  Text </Text>
+    <Header headerText={'Albums'} />
 );
 AppRegistry.registerComponent('albums', () => App);
